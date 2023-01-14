c29f15ea54ca4880feb2b78ab8fd1d9d89eea914
Aymeric Giraudet <aymeric.giraudet@algolia.com>
2023-01-14 17:35:13 +0100

    feat(Pagination): added rest of navigation links

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -4,7 +4,7 @@
   import connect from "../connect";
 
   const state = connect(connectPagination);
-  $: ({ nbPages, currentRefinement, createURL, refine } = $state);
+  $: ({ nbPages, pages, currentRefinement, createURL, refine, isFirstPage, isLastPage } = $state);
 
   const firstPageIndex = 0;
   $: previousPageIndex = currentRefinement - 1;
@@ -20,7 +20,7 @@
 <div class="ais-Pagination" class:ais-Pagination--noRefinement={currentRefinement <= 1}>
   <ul class="ais-Pagination-list">
     {#if showFirst}
-      <li class="ais-Pagination-item--firstPage">
+      <li class="ais-Pagination-item--firstPage" class:ais-Pagination-item--disabled={isFirstPage}>
         <a
           class="ais-Pagination-link"
           href={createURL(firstPageIndex)}
@@ -33,7 +33,10 @@
         </a>
       </li>{/if}
     {#if showPrevious}
-      <li class="ais-Pagination-item--previousPage">
+      <li
+        class="ais-Pagination-item--previousPage"
+        class:ais-Pagination-item--disabled={isFirstPage}
+      >
         <a
           class="ais-Pagination-link"
           href={createURL(previousPageIndex)}
@@ -43,6 +46,48 @@
           }}
         >
           &lt;
+        </a>
+      </li>{/if}
+    {#each pages as page}
+      <li
+        class="ais-Pagination-item"
+        class:ais-Pagination-item--selected={page === currentRefinement}
+      >
+        <a
+          class="ais-Pagination-link"
+          href={createURL(page)}
+          on:click={(e) => {
+            e.preventDefault();
+            refine(page);
+          }}
+        >
+          {page + 1}
+        </a>
+      </li>{/each}
+    {#if showNext}
+      <li class="ais-Pagination-item--nextPage" class:ais-Pagination-item--disabled={isLastPage}>
+        <a
+          class="ais-Pagination-link"
+          href={createURL(nextPageIndex)}
+          on:click={(e) => {
+            e.preventDefault();
+            refine(nextPageIndex);
+          }}
+        >
+          &gt;
+        </a>
+      </li>{/if}
+    {#if showLast}
+      <li class="ais-Pagination-item--lastPage" class:ais-Pagination-item--disabled={isLastPage}>
+        <a
+          class="ais-Pagination-link"
+          href={createURL(lastPageIndex)}
+          on:click={(e) => {
+            e.preventDefault();
+            refine(lastPageIndex);
+          }}
+        >
+          &gt;&gt;
         </a>
       </li>{/if}
   </ul>
